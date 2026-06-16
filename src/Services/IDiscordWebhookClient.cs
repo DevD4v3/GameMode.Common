018ba8f9fb0fed0122660751d3bec435ec1a44ed
MrDave1999 <daveseva2010@hotmail.es>
2026-06-16 18:15:16 -0500

    feat: add Discord webhook abstractions

---
+++ src/Services/IDiscordWebhookClient.cs
@@ -0,0 +1,27 @@
+﻿namespace GameMode.Common;
+
+/// <summary>
+/// Represents a message to be sent through a Discord webhook.
+/// </summary>
+/// <param name="Content">
+/// The message content.
+/// </param>
+public record DiscordMessage(string Content);
+
+/// <summary>
+/// Provides methods to send notifications through a Discord webhook.
+/// </summary>
+public interface IDiscordWebhookClient
+{
+    /// <summary>
+    /// Sends a message to a Discord webhook.
+    /// </summary>
+    /// <param name="message">
+    /// The message to send.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the message was sent successfully;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    Task<bool> SendAsync(DiscordMessage message);
+}
