15413b6b0feb9d96c61cbfc319816cafa5f6518e
MrDave1999 <daveseva2010@hotmail.es>
2026-07-04 07:38:27 -0500

    feat: add GTA San Andreas weapon definitions

---
+++ src/Weapons/IWeapon.cs
@@ -0,0 +1,27 @@
+﻿namespace GameMode.Common;
+
+/// <summary>
+/// Represents a weapon available in GTA: San Andreas.
+/// </summary>
+public interface IWeapon
+{
+    /// <summary>
+    /// Represents an unlimited amount of ammunition.
+    /// </summary>
+    const int UnlimitedAmmo = 99999999;
+
+    /// <summary>
+    /// Gets the underlying game weapon identifier.
+    /// </summary>
+    Weapon Id { get; }
+
+    /// <summary>
+    /// Gets the display name of the weapon.
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// Gets the weapon slot.
+    /// </summary>
+    int Slot { get; }
+}
---
+++ src/Weapons/WeaponDefinitions.cs
@@ -0,0 +1,67 @@
+﻿namespace GameMode.Common;
+
+/// <summary>
+/// Provides the canonical definitions of GTA: San Andreas weapons.
+/// </summary>
+/// <remarks>
+/// Acts as the single source of truth for weapon metadata, such as
+/// display names and weapon slots.
+/// </remarks>
+public class WeaponDefinitions
+{
+    public static readonly IWeapon Brassknuckle = new GtaWeapon(Weapon.Brassknuckle, 0);
+    public static readonly IWeapon GolfClub = new GtaWeapon(Weapon.Golfclub, 1);
+    public static readonly IWeapon Nitestick = new GtaWeapon(Weapon.Nitestick, 1);
+    public static readonly IWeapon Knife = new GtaWeapon(Weapon.Knife, 1);
+    public static readonly IWeapon Chainsaw = new GtaWeapon(Weapon.Chainsaw, 1);
+    public static readonly IWeapon Parachute = new GtaWeapon(Weapon.Parachute, 11);
+
+    public static readonly IWeapon Colt45 = new GtaWeapon(Weapon.Colt45, "Colt 45", 2);
+    public static readonly IWeapon Silenced = new GtaWeapon(Weapon.Silenced, 2);
+    public static readonly IWeapon Deagle = new GtaWeapon(Weapon.Deagle, 2);
+
+    public static readonly IWeapon Shotgun = new GtaWeapon(Weapon.Shotgun, 3);
+    public static readonly IWeapon CombatShotgun = new GtaWeapon(Weapon.CombatShotgun, "Combat Shotgun", 3);
+    public static readonly IWeapon Sawedoff = new GtaWeapon(Weapon.Sawedoff, 3);
+
+    public static readonly IWeapon MP5 = new GtaWeapon(Weapon.MP5, 4);
+    public static readonly IWeapon Uzi = new GtaWeapon(Weapon.Uzi, 4);
+    public static readonly IWeapon Tec9 = new GtaWeapon(Weapon.Tec9, "Tec-9", 4);
+
+    public static readonly IWeapon AK47 = new GtaWeapon(Weapon.AK47, "AK-47", 5);
+    public static readonly IWeapon M4 = new GtaWeapon(Weapon.M4, 5);
+
+    public static readonly IWeapon SniperRifle = new GtaWeapon(Weapon.Sniper, "Sniper Rifle", 6);
+    public static readonly IWeapon CountryRifle = new GtaWeapon(Weapon.Rifle, "Country Rifle", 6);
+
+    public static readonly IWeapon RocketLauncher = new GtaWeapon(Weapon.RocketLauncher, "Rocket Launcher", 7);
+    public static readonly IWeapon Heatseeker = new GtaWeapon(Weapon.HeatSeeker, "Heatseeker Rocket", 7);
+    public static readonly IWeapon Flamethrower = new GtaWeapon(Weapon.FlameThrower, "Flamethrower", 7);
+    public static readonly IWeapon Minigun = new GtaWeapon(Weapon.Minigun, 7);
+
+    public static readonly IWeapon Grenade = new GtaWeapon(Weapon.Grenade, 8);
+    public static readonly IWeapon TearGas = new GtaWeapon(Weapon.Teargas, "Tear Gas", 8);
+    public static readonly IWeapon Molotov = new GtaWeapon(Weapon.Moltov, "Molotov Cocktail", 8);
+    public static readonly IWeapon SatchelCharge = new GtaWeapon(Weapon.SatchelCharge, "Satchel Charge", 8);
+
+    private class GtaWeapon : IWeapon
+    {
+        public Weapon Id { get; }
+        public string Name { get; }
+        public int Slot { get; }
+
+        public GtaWeapon(Weapon id, int slot)
+        {
+            Id = id;
+            Name = id.ToString();
+            Slot = slot;
+        }
+
+        public GtaWeapon(Weapon id, string name, int slot)
+        {
+            Id = id;
+            Name = name;
+            Slot = slot;
+        }
+    }
+}
